fa63517e052400dd99b02d503819dcbdfec14f38
andyvega <andy.vega@adventistas.org>
2025-01-29 12:59:12 -0500

    estructura del proyecto and props

---
+++ composition-app/src/pokemons/pages/PokemonById.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Pokemon by id</h1>
+  </div>
+</template>
---
+++ composition-app/src/shared/components/NavBar.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+
+interface Props {
+  title?: string;
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+  <nav>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" />
+    <!-- <span v-if="$props.title">{{ $props.title }}</span> -->
+    <span>{{ $props.title || 'CompositionApp' }}</span>
+
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/counter">Counter</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  align-items: flex-start;
+  display: flex;
+  font-size: 12px;
+  margin-top: 10px;
+  text-align: center;
+  width: 100%;
+}
+
+img {
+  margin-right: 5px;
+}
+
+span {
+  margin-right: 10px;
+}
+
+a {
+  margin-right: 5px;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
